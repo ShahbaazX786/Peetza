29ba190f430e68a4f628845cbd6fe6ed1a897b9e
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-10-05 13:32:22 +0530

    CL:1.6- Added 3 component layouts and updated routes in routing module.

--- peetza/src/app/app-routing.module.ts
+++ peetza/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { CartComponent } from './components/cart/cart.component';
+import { BuildComponent } from './components/build/build.component';
+import { OrderComponent } from './components/order/order.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',redirectTo:'',component:HomeComponent},
+  {path:'order',component:OrderComponent},
+  {path:'build',component:BuildComponent},
+  {path:'cart',component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- peetza/src/app/app.module.ts
+++ peetza/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BuildComponent } from './components/build/build.component';
+import { OrderComponent } from './components/order/order.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BuildComponent,
+    OrderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
