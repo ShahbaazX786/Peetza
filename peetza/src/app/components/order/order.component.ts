e534a2579582474245e18d3bfe79621ac325abec
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-10-07 17:22:20 +0530

    CL:2.8 - Used the service to make a request via service to express on readonly URL and it gets the data from the database and stores in the array in service which we again  call in components as a function and then subscribe to the observable (service always returns observable object result) and then we add the array to dataarray which is present in component file.

--- peetza/src/app/components/order/order.component.ts
+++ peetza/src/app/components/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from 'src/app/services/pizza.service';
+
+import Pizza from 'src/app/services/models/pizza';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pizzaService:PizzaService) { }
+
+  dataarray:Pizza[]=[];
 
   ngOnInit(): void {
+    this.getPizzaList();
   }
 
+  getPizzaList(){
+    return this.pizzaService.getPizza().subscribe((res)=>{
+      this.pizzaService.pizzaarray = res as Pizza[];
+      this.dataarray=this.pizzaService.pizzaarray;
+      // console.log(this.dataarray);
+      // console.log(this.pizzaService.pizzaarray);
+    });
+  }
 }
